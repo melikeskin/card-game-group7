68860f6df99e7e4c5d8c9d14c3bad22f32d5be52
kbartukomurcu@gmail.com <kbartukomurcu@gmail.com@d3c41a14-c93c-cc7a-c822-b5f4d7cd23f3>
2015-05-14 08:24:55 +0000

    Entity classes and the MainMenu and ViewCardsMenu has been added. In the ViewCardsMenu, the cards should be added to form a deck. Test class also added and it should be deleted after the test ends (test class name "deneme").

---
+++ MinionCard.java
@@ -0,0 +1,26 @@
+
+public class MinionCard extends Card {
+
+	private int damage;
+	private int health;
+	
+	public void setDamage(int damage)
+	{
+		this.damage = damage;
+	}
+	
+	public int getDamage()
+	{
+		return damage;
+	}
+	
+	public void setHealth(int health)
+	{
+		this.health = health;
+	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+}
